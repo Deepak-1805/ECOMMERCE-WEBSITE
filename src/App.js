ca907162db6057f4d9875a158af2658dfe218a0c
Deepak <deepak26011950@gmail.com>
2022-05-30 13:04:21 +0530

    update login.js

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import './App.css';
 import Navbar from './Components/Navbar';
 import Postnav from './Components/Postnav';
 import Signup from './Components/signup';
-
+import Login from './Components/Login';
 function App() {
   return (
     <>
     <Navbar/>
     <Postnav/>
-     <Signup/>  
+     <Signup/> 
+     <Login/> 
         </>
   );
 }
